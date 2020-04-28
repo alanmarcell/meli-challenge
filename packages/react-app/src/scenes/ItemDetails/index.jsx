fd5ac075f4b0a38113fc56339fc1f9145e304f03
Alan Marcell <alanmarcell@live.com>
2020-04-28 10:13:47 -0300

    loading

--- packages/react-app/src/scenes/ItemDetails/index.jsx
+++ packages/react-app/src/scenes/ItemDetails/index.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from 'react-router-dom';
+import { itemDetailsDatasource } from '../../datasource/meli.datasource';
 
 export default () => {
+  const { id } = useParams();
+  const [details, setDetails] = React.useState({});
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      const searchDatasourceRes = await itemDetailsDatasource(id);
+
+      setDetails(searchDatasourceRes.data.items);
+      setIsLoading(false);
+    }
+    if (id) {
+      fetchData();
+    }
+  });
+
+  if (isLoading) {
+    return <>Loading</>;
+  }
+
   return <>ITEM DETAILS</>;
 };
